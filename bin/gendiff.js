d7b1a01c3c477c9a80dda77c6c0199d97e6eedc7
Aleksey Yamkin <me@jamkin.ru>
2025-08-10 11:13:42 +0300

    Made some edits in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,12 @@ import pkg from '../package.json' with { type: 'json' }
 const program = new Command()
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version(pkg.version, '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command')
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version(pkg.version, '-V, --version', 'output the version number')
+    .helpOption('-h, --help', 'display help for command')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
 
 program.parse()
--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,20 @@ import pluginJs from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default [
-  pluginJs.configs.recommended,
-  stylistic.configs.recommended,
-  {
-    files: ['**/*.{js,ts,tsx}'],
-    ignores: ['dist/**', 'node_modules/**'],
+    pluginJs.configs.recommended,
+    stylistic.configs.recommended,
+    {
+        files: ['**/*.{js,ts,tsx}'],
+        ignores: ['dist/**', 'node_modules/**'],
 
-    languageOptions: {
-      sourceType: 'module',
-      ecmaVersion: 'latest',
-      globals: { console: 'readonly' },
+        languageOptions: {
+            sourceType: 'module',
+            ecmaVersion: 'latest',
+            globals: { console: 'readonly' },
+        },
+
+        rules: {
+            '@stylistic/indent': ['error', 4, { SwitchCase: 1 }],
+        },
     },
-  },
 ]
