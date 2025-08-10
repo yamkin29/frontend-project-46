759b84c52be2ae74faed2057609b7791ebd8a083
Aleksey Yamkin <me@jamkin.ru>
2025-08-10 12:18:05 +0300

    Made diff objects.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import { readFile } from './fs.js'
 import parse from './parsers.js'
 
-export default function genDiff(filepath1, filepath2/* , format */) {
+const formatValue = value => String(value)
+
+const buildDiff = (obj1, obj2) => {
+    const keys = Array.from(new Set([...Object.keys(obj1), ...Object.keys(obj2)])).sort()
+
+    const lines = keys.flatMap((key) => {
+        const in1 = Object.prototype.hasOwnProperty.call(obj1, key)
+        const in2 = Object.prototype.hasOwnProperty.call(obj2, key)
+
+        if (in1 && in2) {
+            if (obj1[key] === obj2[key]) {
+                return [`    ${key}: ${formatValue(obj1[key])}`]
+            }
+            return [
+                `  - ${key}: ${formatValue(obj1[key])}`,
+                `  + ${key}: ${formatValue(obj2[key])}`,
+            ]
+        }
+
+        if (in1) return [`  - ${key}: ${formatValue(obj1[key])}`]
+        return [`  + ${key}: ${formatValue(obj2[key])}`]
+    })
+
+    return ['{', ...lines, '}'].join('\n')
+}
+
+export default function genDiff(filepath1, filepath2 /* , format */) {
     const data1 = parse(readFile(filepath1), filepath1)
     const data2 = parse(readFile(filepath2), filepath2)
-
-    void data1
-    void data2
-    return ''
+    return buildDiff(data1, data2)
 }
